3b5b176d852b617b281865e1b7c84a97effeeb61
EJlearner <rzrshrp@gmail.com>
2017-07-19 21:33:15 -0400

    add page not found component

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Why extends Component {
+  render() {
+    return (
+      <div id='leftcontent'>
+        <h1><span>Page Not FOUND</span></h1>
+        <p>Sorry, the page that you were looking for was not found. Please click a link at the top to navigate to a page.</p>
+      </div>
+    );
+  }
+}
+
+export default Why;
--- src/PageContent/PageContent.js
+++ src/PageContent/PageContent.js
@@ -4,6 +4,7 @@ import {Route, Switch} from 'react-router-dom';
 import Calendar from '../Calendar/Calendar'
 import Home from '../Home/Home'
 import IdeaForm from '../IdeaForm/IdeaForm'
+import NotFound from '../Components/NotFound/NotFound'
 import What from '../What/What'
 import Where from '../Where/Where'
 import WhoWeAre from '../WhoWeAre/WhoWeAre'
@@ -15,12 +16,14 @@ class PageContent extends Component {
     return (
       <Switch>
           <Route exact path="/" component={Home}/>
+          <Route path="/christianedu.html" component={Home}/>
           <Route path="/calendar" component={Calendar}/>
           <Route path="/who" component={WhoWeAre}/>
           <Route path="/why" component={Why}/>
           <Route path="/where" component={Where}/>
           <Route path="/what" component={What}/>
           <Route path="/ideaform" component={IdeaForm}/>
+          <Route component={NotFound}/>
       </Switch>
     );
   }
