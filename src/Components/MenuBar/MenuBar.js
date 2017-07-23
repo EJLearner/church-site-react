b8935842f635977eee8e2a379214094eed7636a5
EJlearner <rzrshrp@gmail.com>
2017-07-22 21:51:53 -0400

    add titlebar and restyle menu bar

--- src/Components/MenuBar/MenuBar.js
+++ src/Components/MenuBar/MenuBar.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import churchLogo from './chrisedtopbanner.png'
+
+import styles from './MenuBar.css'
 
 class MenuBar extends Component {
   render() {
     return (
       <div id="menubar">
         <ul className="cfm">
-          <li><Link to="/">Welcome</Link></li>
+          <li><Link to="/">Home</Link></li>
           <li><Link to="/who">Who We are</Link></li>
           <li><Link to="/why">Why We Are Here</Link></li>
           <li><Link to="/where">Where We Are Going</Link></li>
@@ -14,6 +17,9 @@ class MenuBar extends Component {
           <li className='second-to-last-menu-item'><Link to="/what">What's In Store</Link></li>
           <li className="lastmenuitem"><Link to="/ideaform">Tell Us What You Think</Link></li>
         </ul>
+        <div className="logo">
+          <img src={churchLogo} />
+        </div>
       </div>
     );
   }
