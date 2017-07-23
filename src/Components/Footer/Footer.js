680327990fb56968b0ddc026f06468e889c74060
EJlearner <rzrshrp@gmail.com>
2017-07-23 14:40:08 -0400

    remove unusedi mports for Footer.js (lint)

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import fbLogo from './smfbblack.png'
-import twitterLogo from './smtwblack.png'
-import envelope from './smmailblack.png'
-
-
 class Footer extends Component {
 
   render() {
