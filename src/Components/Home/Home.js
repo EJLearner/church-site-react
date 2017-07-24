0518c78ebab949a42108a2ac32efe05a12a787c1
EJlearner <rzrshrp@gmail.com>
2017-07-24 19:47:46 -0400

    move home styles to own file

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
 import Slider from '../Slider/Slider';
 
-class Home extends Component {
-  render() {
-    return (
-      <Slider showPictureSelectButtons={false} />
-    );
-  }
+const Home = () => {
+  return (
+    <Slider showPictureSelectButtons={false} />
+  );
 }
 
 export default Home;
