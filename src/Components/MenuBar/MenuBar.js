e832c8accb60b70a13256f80d8b0a1a1a21d92d1
EJlearner <rzrshrp@gmail.com>
2017-07-26 21:44:39 -0400

    render link to current page with 'current-page-link'

--- src/Components/MenuBar/MenuBar.js
+++ src/Components/MenuBar/MenuBar.js
@@ -1,21 +1,53 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import { withRouter } from 'react-router-dom'
 import churchLogo from './chrisedtopbanner.png'
 
 import styles from './MenuBar.css'
 
 class MenuBar extends Component {
   render() {
+    console.log(this.props.location);
+
+    const links = [
+      {
+        path: '/',
+        text: 'Home'
+      }, {
+        path: '/who',
+        text: `Who We are`,
+      }, {
+        path: '/why',
+        text: `Why We Are Here`,
+      }, {
+        path: '/where',
+        text: `Where We Are Going`,
+      }, {
+        path: '/calendar',
+        text: `Calendar`,
+      }, {
+        path: '/what',
+        text: `What's In Store`,
+      }, {
+        path: '/ideaform',
+        text: `Tell Us What You Think`,
+      },
+    ];
+
+    const renderedLinks = links.map((link) => {
+      console.log(link.path, this.props.location.pathname)
+      const className = link.path === this.props.location.pathname ? 'current-page-link' : null;
+      return (
+        <li key={link.path} className={className}>
+          <Link to={link.path}>{link.text}</Link>
+        </li>
+      );
+    })
+
     return (
       <div id="menubar">
         <ul className="cfm">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/who">Who We are</Link></li>
-          <li><Link to="/why">Why We Are Here</Link></li>
-          <li><Link to="/where">Where We Are Going</Link></li>
-          <li><Link to='/calendar'>Calendar</Link></li>
-          <li className='second-to-last-menu-item'><Link to="/what">What's In Store</Link></li>
-          <li className="lastmenuitem"><Link to="/ideaform">Tell Us What You Think</Link></li>
+          {renderedLinks}
         </ul>
         <div className="logo">
           <img src={churchLogo}  alt=""/>
@@ -25,4 +57,4 @@ class MenuBar extends Component {
   }
 }
 
-export default MenuBar;
+export default withRouter(MenuBar);
