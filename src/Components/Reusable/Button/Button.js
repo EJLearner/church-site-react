e887abf9a41120f7f855c25812086b3d5b246a78
EJlearner <rzrshrp@gmail.com>
2017-12-13 09:47:54 -0500

    finish button component changes and styling

--- src/Components/Reusable/Button/Button.js
+++ src/Components/Reusable/Button/Button.js
@@ -3,19 +3,40 @@ import PropTypes from 'prop-types';
 
 import './Button.css';
 
+const STYLES = {
+  OVAL: 'OVAL',
+  RECT: 'RECT'
+};
+
 class Button extends Component {
   render() {
+    const {className, onClick, children, style} = this.props;
+    let computedClassName =
+      style === STYLES.RECT ? 'rect-button' : 'oval-button';
+
+    if (className) {
+      computedClassName += ` ${className}`;
+    }
+
     return (
-      <button className="button-component" onClick={this.props.onClick}>
-        {this.props.children}
+      <button className={computedClassName} onClick={onClick}>
+        {children}
       </button>
     );
   }
 }
 
-Button.propTypes = {
-  children: PropTypes.any,
-  onClick: PropTypes.func.isRequired
+Button.defaultProps = {
+  style: STYLES.OVAL
 };
+
+Button.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.any,
+  onClick: PropTypes.func.isRequired,
+  style: PropTypes.string
+};
+
+Button.STYLES = STYLES;
 
 export default Button;
