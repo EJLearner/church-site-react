706f3f8890479b877ad30282811cab8867e2653f
EJlearner <rzrshrp@gmail.com>
2018-05-31 20:16:51 -0400

    add tests

--- src/ce/components/RegistrationPages/BaseRegistrationChild.test.js
+++ src/ce/components/RegistrationPages/BaseRegistrationChild.test.js
@@ -2,16 +2,59 @@ import BaseRegistrationChild from './BaseRegistrationChild';
 import {shallow} from 'enzyme';
 import React from 'react';
 import routePaths from '../../../routePaths';
+import registrationUtils from './registrationUtils';
 jest.mock('firebase');
+jest.mock('./registrationUtils.js');
 
-it('should redirect if redirect state is truthy', () => {
-  const wrapper = shallow(<BaseRegistrationChild refName="testRefname" />);
-  wrapper.setState({redirect: true});
+describe('BaseRegistrationChild', () => {
+  let props;
 
-  const redirect = wrapper.find('Redirect');
+  beforeEach(() => {
+    jest.mock('firebase');
+    jest.mock('./registrationUtils.js');
 
-  expect(redirect.exists()).toBeTruthy();
-  expect(redirect.props().push).toEqual(true);
-  expect(redirect.props().to.pathname).toEqual(routePaths.CE_THANK_YOU);
-  expect(redirect.props().to.state.forMessage).toEqual('you for registering.');
+    props = {
+      className: 'test-class',
+      headerContent: <h1 id="header-content">test-header</h1>,
+      refName: 'test-ref-name'
+    };
+  });
+
+  afterEach(() => {
+    registrationUtils.renderErrors.mockClear();
+  });
+
+  it('should redirect if redirect state is truthy', () => {
+    const wrapper = shallow(<BaseRegistrationChild {...props} />);
+    wrapper.setState({redirect: true});
+
+    const redirect = wrapper.find('Redirect');
+
+    expect(redirect.exists()).toBeTruthy();
+    expect(redirect.props().push).toEqual(true);
+    expect(redirect.props().to.pathname).toEqual(routePaths.CE_THANK_YOU);
+    expect(redirect.props().to.state.forMessage).toEqual(
+      'you for registering.'
+    );
+  });
+
+  it('should render outer div with className from prop', () => {
+    const wrapper = shallow(<BaseRegistrationChild {...props} />);
+
+    expect(wrapper.find(`div.${props.className}`).exists()).toEqual(true);
+  });
+
+  it('renders headerContent', () => {
+    const wrapper = shallow(<BaseRegistrationChild {...props} />);
+
+    expect(wrapper.containsMatchingElement(props.headerContent)).toEqual(true);
+  });
+
+  it('renders errors', () => {
+    const wrapper = shallow(<BaseRegistrationChild {...props} />);
+
+    expect(registrationUtils.renderErrors).toHaveBeenCalledTimes(1);
+
+    expect(wrapper.containsMatchingElement(props.headerContent)).toEqual(true);
+  });
 });
