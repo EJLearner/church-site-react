8ad958c0335c3255626e58364716fbb480c63394
EJlearner <rzrshrp@gmail.com>
2018-09-09 22:25:55 -0400

    add cc registered volunteers ref constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,18 @@
 const constants = {
   VALID_INPUT_DATE_FORMATS: ['M/D/YY', 'M/D/YYYY', 'M-D-YYYY', 'M-D-YY'],
   INTERNAL_DATE_FORMAT: 'YYYY-MM-DD',
+
   VBS_LOGBOOK_REF_NAME: 'vbsLogbook',
   VBS_REGISTERED_CHILD_ID_PROP: 'vbsRegisteredChildrenId',
   VBS_REGISTERED_CHILDREN_REF_NAME: 'vbsRegisteredChildren',
   VBS_REGISTRY_ACCESS_REF_NAME: 'user_groups/vbsRegAccess',
+
   CC_LOGBOOK_REF_NAME: 'ccLogbook',
   CC_REGISTRY_ACCESS_REF_NAME: 'user_groups/ccRegAccess',
   CC_REGISTERED_CHILD_ID_PROP: 'ccRegisteredId',
-  CC_REGISTERED_CHILDREN_REF_NAME: 'ccRegisteredChildren'
+  CC_REGISTERED_CHILDREN_REF_NAME: 'ccRegisteredChildren',
+
+  CC_REGISTERED_VOLUNTEER_REF_NAME: 'ccRegisteredVolunteers'
 };
 
 export default constants;
