9ef701b1d0ee5f27ae43453c5569b2c422cd4387
EJlearner <rzrshrp@gmail.com>
2018-09-14 20:48:32 -0400

    update vbscheckin page and add tests

---
+++ src/ce/components/CcVbsCheckInOut/VbsCheckin.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import constants from '../../../utils/constants';
+import routePaths from '../../../routePaths';
+
+import {shallow} from 'enzyme';
+
+import VbsCheckin from './VbsCheckin';
+import {expect} from 'chai';
+import utils from '../../../utils/commonUtils';
+
+describe('#render', () => {
+  it('renders BaseCheckin', () => {
+    const wrapper = shallow(<VbsCheckin />);
+    expect(wrapper.find('BaseCheckin').exists()).to.be.true;
+  });
+
+  it('BaseCheckin has correct properties', () => {
+    const testYear = '2000';
+    utils.getVbsDbYear = jest.fn(() => testYear);
+
+    const wrapper = shallow(<VbsCheckin />);
+    const baseCheckin = wrapper.find('BaseCheckin');
+
+    expect(baseCheckin.props().logbookRefName).to.equal(
+      constants.VBS_LOGBOOK_REF_NAME
+    );
+
+    expect(baseCheckin.props().registerLink).to.equal(
+      routePaths.CE_VBS_REG_CHILD
+    );
+
+    expect(baseCheckin.props().registeredChildrenRefName).to.equal(
+      `${constants.VBS_REGISTERED_CHILDREN_REF_NAME}/${testYear}`
+    );
+
+    expect(baseCheckin.props().registryAccessRefName).to.equal(
+      constants.VBS_REGISTRY_ACCESS_REF_NAME
+    );
+
+    expect(baseCheckin.props().registryIdName).to.equal(
+      constants.VBS_REGISTERED_CHILD_ID_PROP
+    );
+
+    expect(baseCheckin.props().welcomeName).to.equal('Vacation Bible School');
+  });
+});
--- src/ce/components/CcVbsCheckinOut/VbsCheckin.js
+++ src/ce/components/CcVbsCheckinOut/VbsCheckin.js
@@ -3,14 +3,19 @@ import constants from '../../../utils/constants';
 import routePaths from '../../../routePaths';
 
 import BaseCheckin from './BaseCheckin';
+import utils from '../../../utils/commonUtils';
 
 class VbsCheckin extends Component {
   render() {
+    const year = utils.getVbsDbYear();
+
     return (
       <BaseCheckin
         logbookRefName={constants.VBS_LOGBOOK_REF_NAME}
         registerLink={routePaths.CE_VBS_REG_CHILD}
-        registeredChildrenRefName={constants.VBS_REGISTERED_CHILDREN_REF_NAME}
+        registeredChildrenRefName={`${
+          constants.VBS_REGISTERED_CHILDREN_REF_NAME
+        }/${year}`}
         registryAccessRefName={constants.VBS_REGISTRY_ACCESS_REF_NAME}
         registryIdName={constants.VBS_REGISTERED_CHILD_ID_PROP}
         welcomeName="Vacation Bible School"
