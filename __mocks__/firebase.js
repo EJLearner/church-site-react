641f150a2c149bd91db6bbbb406959453d6e1a48
EJlearner <rzrshrp@gmail.com>
2019-08-02 22:37:47 -0400

    add firebase mock

---
+++ __mocks__/firebase.js
@@ -0,0 +1,40 @@
+const firebase = jest.genMockFromModule('firebase');
+
+const set = jest.fn(() => {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-constant-condition
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+});
+
+const refObject = {
+  child: jest.fn(function() {
+    return {
+      set: set
+    };
+  }),
+  on: jest.fn(),
+  set: jest.fn(),
+  update: jest.fn()
+};
+
+const refStub = jest.fn(function() {
+  return refObject;
+});
+
+const dbObject = {
+  ref: refStub
+};
+
+const getDatabase = function() {
+  return dbObject;
+};
+
+firebase.initializeApp = jest.fn();
+firebase.database = jest.fn(getDatabase);
+
+module.exports = firebase;
