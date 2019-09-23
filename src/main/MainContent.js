d16c6501e82d0506b3999d3b5d8b49155c16647a
EJlearner <rzrshrp@gmail.com>
2019-09-22 22:05:08 -0400

    add placeholder slider and services streaming link

--- src/main/MainContent.js
+++ src/main/MainContent.js
@@ -1,15 +1,61 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Slider from '../common/Slider';
+
+import routePaths from '../routePaths';
+
+import './MainContent.css';
+
+const allPictures = [
+  {
+    altTag: 'Christ Is Risen: Celebrate the Savior',
+    annual: true,
+    displayBeg: '2018-03-01',
+    displayEnd: '2018-05-01',
+    source: require('../assets/ce/images/slides/christian-ed-home-christ-risen.png')
+  },
+  {
+    altTag:
+      'o be a good father and mother requires that the parents defer many of ' +
+      'their own needs and desires in favor of the needs of their children. As a ' +
+      'consequence of this sacrifice, conscientious parents develop a nobility of ' +
+      'character and learn to put into practice the selfless truths taught by the Savior Himself. Quote from Robert Faust' +
+      'Mother’s Day Service May 13' +
+      'Father’s Day Service June 17',
+    displayBeg: '2018-05-01',
+    displayEnd: '2018-06-20',
+    source: require('../assets/ce/images/slides/christian-ed-home-mothers-fathers.png')
+  },
+  {
+    altTag: 'Children’s Church - Every Second Sunday',
+    source: require('../assets/ce/images/slides/home-childrens-church.png')
+  },
+  {
+    altTag: 'Bible Study - Every Tuesday at 7:30 PM and Every Saturday at Noon',
+    source: require('../assets/ce/images/slides/christian-ed-home-bible-study.png')
+  }
+];
 
 class MainContent extends React.Component {
   render() {
     return (
-      <ul>
-        <li>Slideshow</li>
-        <li>Stream Box</li>
-        <li>Two pictures and calendar</li>
-        <li>Daily devotional</li>
-        <li>Upcoming Events</li>
-      </ul>
+      <>
+        <Slider pictures={allPictures} />
+        <div className="stream-services">
+          <Link to={routePaths.MAIN_HOME}>
+            Missed A Sunday? Stream Our services
+            <br />
+            <span className="further-info">
+              Stream current Sunday service or download past sermons.
+            </span>
+          </Link>
+        </div>
+        <ul>
+          <li>Two pictures and calendar</li>
+          <li>Daily devotional</li>
+          <li>Upcoming Events</li>
+        </ul>
+      </>
     );
   }
 }
--- src/main/MainContent.test.js
+++ src/main/MainContent.test.js
@@ -7,5 +7,45 @@ describe('MainContent', () => {
     expect(shallow(<MainContent />).exists()).toBe(true);
   });
 
-  it('renders slideshow', () => {});
+  it('renders slideshow', () => {
+    const wrapper = shallow(<MainContent />);
+
+    expect(wrapper.find('Slider').exists()).toBe(true);
+  });
+
+  it('renders stream div', () => {
+    const wrapper = shallow(<MainContent />);
+
+    expect(wrapper.find('.stream-services').exists()).toBe(true);
+  });
+
+  // it('renders outreach div', () => {
+  //   const wrapper = shallow(<MainContent />);
+
+  //   expect(wrapper.find('.outreach-services').exists()).toBe(true);
+  // });
+
+  // it('renders grow with us div', () => {
+  //   const wrapper = shallow(<MainContent />);
+
+  //   expect(wrapper.find('.grow-with-us').exists()).toBe(true);
+  // });
+
+  // it('renders calendar link', () => {
+  //   const wrapper = shallow(<MainContent />);
+
+  //   expect(wrapper.find('.calendar-link').exists()).toBe(true);
+  // });
+
+  // it('renders daily devotional link', () => {
+  //   const wrapper = shallow(<MainContent />);
+
+  //   expect(wrapper.find('.daily-devotional').exists()).toBe(true);
+  // });
+
+  // it('renders upcoming events', () => {
+  //   const wrapper = shallow(<MainContent />);
+
+  //   expect(wrapper.find('.upcoming-events').exists()).toBe(true);
+  // });
 });
