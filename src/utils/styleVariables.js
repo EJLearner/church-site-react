52d20c5b893eaf03fb0882a6a6ba00a6ead532a8
EJlearner <rzrshrp@gmail.com>
2019-12-23 23:40:47 -0500

    add js style variables

---
+++ src/utils/styleVariables.js
@@ -0,0 +1,41 @@
+const STYLES = {
+  FONT_FAMILIES: {
+    ARIAL: 'arial, helvetica, sans_serif',
+    CALIBRI: 'Calibri, \'Segoe UI\', Candara, Segoe, Optima, Arial, sans_serif',
+    CAMBRIA: 'cambria, georgia, \'bookman old style\', \'times new roman\', serif',
+    CENTURY_GOTHIC: '\'Century Gothic\', CenturyGothic, AppleGothic, sans_serif',
+    GARAMOND:
+      '\'Adobe Garamond Pro\', Garamond, Baskerville, \'Baskerville Old Face\',' +
+      '\'Hoefler Text\', \'Times New Roman\', serif'
+  },
+
+  COLORS: {
+    GREEN_20: 'rgb(146, 208, 80)',
+    GREEN_50: 'rgb(115, 175, 85)',
+    GREEN_80: 'rgb(102, 158, 74)',
+    ORANGE: 'orange',
+    PEACH: 'rgb(255, 230, 149)',
+    GRAY41: 'rgb(41, 41, 41)',
+    GRAY63: 'rgb(63, 63, 63)',
+    GRAY77: 'rgb(77, 77, 77)',
+    GRAY83: 'rgb(83, 83, 83)',
+    GRAY95: '#5f5f5f',
+    GRAY180: 'rgb(180, 180, 180)',
+    GRAY217: 'rgb(217, 217, 217)',
+    LIGHT_BLUE: '#5b9bd5',
+    BLACK: 'black',
+    WHITE: 'white',
+    RED: 'red',
+    YELLOW: 'yellow',
+    MAROON: 'maroon',
+    GREEN: 'green',
+    GRAY: 'gray'
+  },
+
+  LOGICAL_COLORS: {
+    STANDARD_BACKGROUND: 'white',
+    STANDARD_TEXT: '$black'
+  }
+};
+
+export default STYLES;
