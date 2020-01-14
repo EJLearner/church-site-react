1675187a4bb702a36228aeb8f9439641430b2cd7
EJlearner <rzrshrp@gmail.com>
2020-01-13 23:04:23 -0500

    break JubileeContent out

---
+++ src/main/MainPage/JubileeContent.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import {LOGICAL_COLORS, FONT_FAMILIES} from '../../utils/styleVariables';
+
+const TitleAndContent = styled.div`
+  background-color: ${LOGICAL_COLORS.CT_PRIMARY};
+  color: ${LOGICAL_COLORS.CT_TEXT_ON_PRIMARY};
+  padding: 1em;
+  max-width: 500px;
+`;
+
+const TitlesDiv = styled.div`
+  display: inline-block;
+`;
+
+const FirstLine = styled.h1`
+  font-family: ${FONT_FAMILIES.BRUSH_SCRIPT};
+  margin-bottom: 0;
+  line-height: 0.7;
+  text-align: right;
+`;
+
+const Numbers = styled.span`
+  font-size: 160%;
+`;
+
+const SecondLine = styled.h2`
+  font-style: italic;
+  margin-top: 0;
+  text-align: right;
+`;
+
+function JubileeContent() {
+  return (
+    <TitleAndContent>
+      <TitlesDiv>
+        <FirstLine>
+          <Numbers>
+            50<sup>th</sup>
+          </Numbers>{' '}
+          Anniversary Celebration
+        </FirstLine>
+        <SecondLine>Year of Jubilee!</SecondLine>
+      </TitlesDiv>
+      <p>
+        2020 marks an important milestone in the life of our church. Join us
+        each Sunday as we celebrate and thank God for 50 years of worship,
+        outreach, and praise!
+      </p>
+    </TitleAndContent>
+  );
+}
+
+export default JubileeContent;
--- src/main/MainPage/index.jsx
+++ src/main/MainPage/index.jsx
@@ -6,9 +6,10 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import churchExterior from '../../assets/main/images/church-exterior.png';
 
 import MainMenubar from '../MainMenubar';
-import {LOGICAL_COLORS, FONT_FAMILIES} from '../../utils/styleVariables';
+import {LOGICAL_COLORS} from '../../utils/styleVariables';
 import ScrollingEventsText from './ScrollingEventsText';
 import ContentSelectBoxes from './ContentSelectBoxes';
+import JubileeContent from './JubileeContent';
 
 const MenuBarAndPictureDiv = styled.div`
   background-image: url(${churchExterior});
@@ -19,34 +20,6 @@ const MenuBarAndPictureDiv = styled.div`
 const AnnouncementBox = styled.div`
   margin: 64px 0 0 64px;
   width: 50%;
-`;
-
-const TitleAndContent = styled.div`
-  background-color: ${LOGICAL_COLORS.CT_PRIMARY};
-  color: ${LOGICAL_COLORS.CT_TEXT_ON_PRIMARY};
-  padding: 1em;
-  max-width: 500px;
-`;
-
-const TitlesDiv = styled.div`
-  display: inline-block;
-`;
-
-const FirstLine = styled.h1`
-  font-family: ${FONT_FAMILIES.BRUSH_SCRIPT};
-  margin-bottom: 0;
-  line-height: 0.7;
-  text-align: right;
-`;
-
-const Numbers = styled.span`
-  font-size: 160%;
-`;
-
-const SecondLine = styled.h2`
-  font-style: italic;
-  margin-top: 0;
-  text-align: right;
 `;
 
 const ArrowAndLearnmore = styled.div`
@@ -95,22 +68,7 @@ function MainContent() {
       <MenuBarAndPictureDiv className="menu-bar-and-picture">
         <MainMenubar />
         <AnnouncementBox>
-          <TitleAndContent>
-            <TitlesDiv>
-              <FirstLine>
-                <Numbers>
-                  50<sup>th</sup>
-                </Numbers>{' '}
-                Anniversary Celebration
-              </FirstLine>
-              <SecondLine>Year of Jubilee!</SecondLine>
-            </TitlesDiv>
-            <p>
-              2020 marks an important milestone in the life of our church. Join
-              us each Sunday as we celebrate and thank God for 50 years of
-              worship, outreach, and praise!
-            </p>
-          </TitleAndContent>
+          <JubileeContent />
           <ArrowAndLearnmore>
             <ArrowBox>
               <div>
