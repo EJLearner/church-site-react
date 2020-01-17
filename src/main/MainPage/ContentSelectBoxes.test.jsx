c46d65739b38b0c7fdc0e15bb8481e1d4c4f3c35
EJlearner <rzrshrp@gmail.com>
2020-01-17 17:12:18 -0500

    fix tets and delete snapshot

--- src/main/MainPage/ContentSelectBoxes.test.jsx
+++ src/main/MainPage/ContentSelectBoxes.test.jsx
@@ -5,6 +5,10 @@ import ContentSelectBoxes from './ContentSelectBoxes';
 
 describe('ContentSelectBoxes', () => {
   it('renders', () => {
-    expect(shallow(<ContentSelectBoxes />).exists()).toBe(true);
+    expect(
+      shallow(
+        <ContentSelectBoxes contentIndex={0} onContentSelect={() => {}} />
+      ).exists()
+    ).toBe(true);
   });
 });
