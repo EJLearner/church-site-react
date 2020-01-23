7602f8a359e5f92aa23febdf80cddd95921687d3
EJlearner <rzrshrp@gmail.com>
2020-01-22 21:44:17 -0500

    add structure to aboutus page

--- src/main/AboutUs.jsx
+++ src/main/AboutUs.jsx
@@ -1,5 +1,59 @@
+import React from 'react';
+import GeneralPage from './commonComponents/GeneralPage';
+
+const pastorContent = `
+    Pastor, ipsum dolor sit amet consectetur adipisicing elit. Tempore
+    officia necessitatibus atque molestiae? Eveniet debitis itaque ad iure.
+    Cumque reiciendis eveniet quia fugiat eius nostrum vel doloremque
+    dignissimos, quisquam atque.
+  `;
+
+const membershipContent = `
+    Membership, ipsum dolor sit amet consectetur adipisicing elit. Tempore officia
+    necessitatibus atque molestiae? Eveniet debitis itaque ad iure. Cumque
+    reiciendis eveniet quia fugiat eius nostrum vel doloremque dignissimos,
+    quisquam atque.
+  `;
+
+const historyContent = `
+    History, ipsum dolor sit amet consectetur adipisicing elit. Tempore officia
+    necessitatibus atque molestiae? Eveniet debitis itaque ad iure. Cumque
+    reiciendis eveniet quia fugiat eius nostrum vel doloremque dignissimos,
+    quisquam atque.
+  `;
+
+const bottomContentData = [
+  {
+    title: 'The Pastor',
+    id: 'thePastor',
+    content: pastorContent
+  },
+  {title: 'Membership', id: 'membership', content: membershipContent},
+  {title: 'History', id: 'history', content: historyContent}
+];
+
+const topBoxContent = (
+  <div>
+    <div>
+      <h1>Something About Us</h1>
+    </div>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus
+      magni numquam consequatur vitae sapiente unde mollitia a. Tenetur aut nam
+      reiciendis repellat excepturi itaque voluptatum assumenda consequuntur!
+      Minima, nam tenetur!
+    </p>
+  </div>
+);
+
 const MainAboutUs = () => {
-  return 'About Us';
+  return (
+    <GeneralPage
+      bottomContentData={bottomContentData}
+      menuTitle="About Us"
+      topBoxContent={topBoxContent}
+    />
+  );
 };
 
 export default MainAboutUs;
