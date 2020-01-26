0c38c0609353c6d7fe8a3a0e616061a1d73d58a4
EJlearner <rzrshrp@gmail.com>
2020-01-26 16:52:35 -0500

    content select arrow changes
    show pointer when hovering over arrow
    remove hidden arrow from tabIndex

--- src/main/MainPage/ContentSelectArrow.jsx
+++ src/main/MainPage/ContentSelectArrow.jsx
@@ -6,7 +6,10 @@ import {faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {LOGICAL_COLORS} from '../../utils/styleVariables';
 
-const ArrowIcon = styled.div`
+const ArrowIcon = styled.button`
+  border: none;
+  cursor: ${props => (props.show ? 'pointer' : 'cursor')};
+  background-color: ${LOGICAL_COLORS.CT_SECOND}
   color: ${props =>
     props.show ? LOGICAL_COLORS.CT_TEXT_ON_SECONDARY : 'transparent'};
   margin: 1em;
@@ -14,9 +17,15 @@ const ArrowIcon = styled.div`
 
 function ContentSelectArrow({onClick, type, show}) {
   const icon = type === 'left' ? faAngleLeft : faAngleRight;
+  const tabIndex = show ? null : -1;
 
   return (
-    <ArrowIcon aria-hidden={!show} onClick={show ? onClick : null} show={show}>
+    <ArrowIcon
+      aria-hidden={!show}
+      onClick={show ? onClick : null}
+      show={show}
+      tabIndex={tabIndex}
+    >
       <FontAwesomeIcon fixedWidth icon={icon} size="4x" />
     </ArrowIcon>
   );
