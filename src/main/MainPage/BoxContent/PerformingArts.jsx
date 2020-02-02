68f79751bd54683c650a3af6585cf8cb68e9a713
April <asdayjones@gmail.com>
2020-02-02 14:08:24 -0500

    improve performing arts blurb wording

--- src/main/MainPage/BoxContent/PerformingArts.jsx
+++ src/main/MainPage/BoxContent/PerformingArts.jsx
@@ -9,7 +9,7 @@ function PerformingArts() {
         enhance our worship experience. Categories of artistic talent include
         solo or ensemble sacred, classical, spiritual, or hymnal jazz vocal and
         instrument; dance; theater; and dramatic readings. Learn more to find
-        out how you can participate or make a referral.
+        out how you can participate or refer a talented artist you know.
       </p>
     </div>
   );
