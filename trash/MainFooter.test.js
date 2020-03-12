b27405edd39253bbf41529cc7d0a8914a0ec56b8
EJlearner <rzrshrp@gmail.com>
2020-03-12 17:58:50 -0400

    clean out unused files

--- trash/MainFooter.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import MainFooter from './MainFooter';
-
-describe('MainHome', () => {
-  it('renders', () => {
-    expect(shallow(<MainFooter />).exists()).toBe(true);
-  });
-});
