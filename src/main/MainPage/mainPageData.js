c99587a15a72880030492b6cdadcf34c6bb13899
EJlearner <rzrshrp@gmail.com>
2020-03-22 10:00:01 -0400

    link to watch page and update worship blurb

--- src/main/MainPage/mainPageData.js
+++ src/main/MainPage/mainPageData.js
@@ -9,42 +9,41 @@ import ChristianEducation from './BoxContent/ChristianEducation';
 import CultureAndFineArts from './BoxContent/CultureAndFineArts';
 import JubileeContent from './BoxContent/JubileeContent';
 
-
 // import GedProgram from './BoxContent/GedProgram';
 import WorshipExperience from './BoxContent/WorshipExperience';
 import CoronaVirusContent from './CoronaVirusContent';
 
 const contentSelectInfo = [
   {
-    linkPath: routePaths.MAIN_CONTACT,
+    linkPath: routePaths.MAIN_WATCH,
     render: WorshipExperience,
-    title: 'Worship Experience',
-    thumbnail: choirThumb
+    thumbnail: choirThumb,
+    title: 'Worship Experience'
   },
   {
     linkPath: routePaths.MAIN_JUBILEE,
     render: JubileeContent,
     showUntil: '2021-01-01',
-    title: '50th Anniversary Celebration',
-    thumbnail: shofarBlower
+    thumbnail: shofarBlower,
+    title: '50th Anniversary Celebration'
   },
   {
     linkPath: routePaths.MAIN_CORONAVIRUS,
     render: CoronaVirusContent,
-    title: 'Coronavirus Update',
-    thumbnail: coronavirus
+    thumbnail: coronavirus,
+    title: 'Coronavirus Update'
   },
   {
     linkPath: routePaths.MAIN_CULTURE_AND_ARTS,
     render: CultureAndFineArts,
-    title: 'Culture & Fine Arts',
-    thumbnail: dance
+    thumbnail: dance,
+    title: 'Culture & Fine Arts'
   },
   {
     linkPath: routePaths.CE_HOME,
     render: ChristianEducation,
-    title: 'Christian Education',
-    thumbnail: christianEdPeopleThumb
+    thumbnail: christianEdPeopleThumb,
+    title: 'Christian Education'
   }
 
   // TODO: add this back later
