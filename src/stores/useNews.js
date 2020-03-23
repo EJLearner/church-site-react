39ce3d9a32ae134cd003faa911701d935d8f6cf4
EJlearner <rzrshrp@gmail.com>
2020-03-23 19:36:32 -0400

    sort objects

--- src/stores/useNews.js
+++ src/stores/useNews.js
@@ -7,46 +7,46 @@ import constants from '../utils/constants';
 function useNews() {
   const allNews = [
     {
-      text:
-        'Due to COVID-19, no in-sanctuary service will be held. Watch Sunday’s sermon.',
+      expireDate: '2020-06-01',
+      id: 'coronavirus-info',
       linkPath: routePaths.MAIN_CORONAVIRUS,
       sortOrder: 1,
-      expireDate: '2020-06-01',
-      title: 'On-line Sermon Posted',
-      id: 'coronavirus-info'
+      text:
+        'Due to COVID-19, no in-sanctuary service will be held. Watch Sunday’s sermon.',
+      title: 'On-line Sermon Posted'
     },
     {
+      expireDate: '2020-06-01',
+      id: 'coronavirus-info',
+      linkPath: routePaths.MAIN_CORONAVIRUS,
+      sortOrder: 1,
       text:
         'Coronavirus information posted. Visit the Coronavirus Update page for information' +
         'on staying safe and changes in the church.',
-      linkPath: routePaths.MAIN_CORONAVIRUS,
-      sortOrder: 1,
-      expireDate: '2020-06-01',
-      title: 'Coronavirus Information',
-      id: 'coronavirus-info'
+      title: 'Coronavirus Information'
     },
     {
-      text:
-        'Stewardship Reports for the 2019 tax year are now available. See Michele Williams for your statement!',
-      linkPath: routePaths.MAIN_HOME,
       expireDate: '2020-03-15',
+      id: 'reports',
+      linkPath: routePaths.MAIN_HOME,
       sortOrder: 20,
-      id: 'reports'
+      text:
+        'Stewardship Reports for the 2019 tax year are now available. See Michele Williams for your statement!'
     },
     {
+      expireDate: '2020-04-04',
+      id: 'anniversary-patrons',
       text:
         'The Ushers and Nurses will be selling patrons for their upcoming anniversary. ' +
         'The cost of the patrons is $2.00 per name. See any usher or nurse for more information. ' +
         'Thank you for your continued support.',
-      expireDate: '2020-04-04',
-      title: 'Anniversary Patrons',
-      id: 'anniversary-patrons'
+      title: 'Anniversary Patrons'
     },
     {
-      text: 'Website redesign is live!',
       expireDate: '2020-05-01',
-      title: 'Website Redesign',
-      id: 'website-redesign'
+      id: 'website-redesign',
+      text: 'Website redesign is live!',
+      title: 'Website Redesign'
     }
   ];
 
