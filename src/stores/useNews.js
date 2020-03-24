cbf5488b041646919ecf9d08763dcf24cc2d25b2
EJlearner <rzrshrp@gmail.com>
2020-03-23 20:45:24 -0400

    require and sort by postedDate

--- src/stores/useNews.js
+++ src/stores/useNews.js
@@ -8,18 +8,18 @@ function useNews() {
   const allNews = [
     {
       expireDate: '2020-06-01',
-      id: 'coronavirus-info',
+      id: 'no-sanctuary-service',
       linkPath: routePaths.MAIN_CORONAVIRUS,
-      sortOrder: 1,
+      postedDate: '2020-03-19',
       text:
         'Due to COVID-19, no in-sanctuary service will be held. Watch Sunday’s sermon.',
       title: 'On-line Sermon Posted'
     },
     {
       expireDate: '2020-06-01',
-      id: 'coronavirus-info',
+      id: 'coronavirus-info-posted',
       linkPath: routePaths.MAIN_CORONAVIRUS,
-      sortOrder: 1,
+      postedDate: '2020-03-19',
       text:
         'Coronavirus information posted. Visit the Coronavirus Update page for information' +
         'on staying safe and changes in the church.',
@@ -29,13 +29,14 @@ function useNews() {
       expireDate: '2020-03-15',
       id: 'reports',
       linkPath: routePaths.MAIN_HOME,
-      sortOrder: 20,
+      postedDate: '2020-03-19',
       text:
         'Stewardship Reports for the 2019 tax year are now available. See Michele Williams for your statement!'
     },
     {
       expireDate: '2020-04-04',
       id: 'anniversary-patrons',
+      postedDate: '2020-03-19',
       text:
         'The Ushers and Nurses will be selling patrons for their upcoming anniversary. ' +
         'The cost of the patrons is $2.00 per name. See any usher or nurse for more information. ' +
@@ -45,13 +46,20 @@ function useNews() {
     {
       expireDate: '2020-05-01',
       id: 'website-redesign',
+      postedDate: '2020-03-19',
       text: 'Website redesign is live!',
       title: 'Website Redesign'
     }
   ];
 
   const filteredNews = allNews.filter(newsItem => {
-    const {expireDate} = newsItem;
+    const {id, expireDate, postedDate} = newsItem;
+
+    if (!postedDate) {
+      throw new Error(
+        `News item with id: ${id} is missing "postedDate". Every news item must have "postedDate" property`
+      );
+    }
 
     const expireMoment = moment(
       expireDate,
@@ -67,7 +75,8 @@ function useNews() {
 
   return commonUtils.sort(
     filteredNews,
-    news => news.sortOrder ?? Number.POSITIVE_INFINITY
+    news => news.postedDate,
+    constants.SORT_DIRECTION_DESCENDING
   );
 }
 
