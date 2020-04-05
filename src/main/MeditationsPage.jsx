344e04f54d65347a91e791a4e77ca6d7e1f92165
EJlearner <rzrshrp@gmail.com>
2020-04-05 16:09:00 -0400

    move weekly meditations to external file

--- src/main/MeditationsPage.jsx
+++ src/main/MeditationsPage.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import Verse from '../common/components/Verse';
 import routePaths from '../routePaths';
 import {bibleComFormattedVerses} from '../stores/dailyVerses';
+import weeklyMeditations from '../stores/weeklyMeditations';
 import constants from '../utils/constants';
 import {FONT_FAMILIES} from '../utils/styleVariables';
 
@@ -90,99 +91,6 @@ function getVersesContent() {
   });
 }
 
-const meditationContent = {
-  '2020-03-29': (
-    <>
-      <h2>
-        Weekly Meditation{' '}
-        <span className="subtitle">
-          From <i>God In Search of Man</i> by Abraham Joshua Herschel
-        </span>
-      </h2>
-
-      <p>
-        The world needs more than the secret holiness of individual inwardness.
-        It needs more than sacred sentiments and good intentions. God asks for
-        the heart because He needs the lives. It is by lives. It is by lives
-        that the world will be redeemed, by lives that beat in concordance with
-        God, by deeds that outbeat the finite charity of the human heart.
-      </p>
-      <p>
-        Man’s power of action is less vague than his power of intention. And an
-        action has intrinsic meaning; its value to the world is independent of
-        what it means to the person performing it. The act of giving food to a
-        helpless child is meaningful regardless of whether or not the moral
-        intention is present. God asks for the heart, and we must sell our
-        answer in terms of deeds.
-      </p>
-      <p>
-        It would be a device of conceit, if not presumption, to insist that
-        purity of the heart is the exclusive test of piety. Perfect purity is
-        something we rarely know how to obtain or how to retain. No one can
-        claim to have purged all the dross even from his finest desire. The self
-        is finite, but selfishness is infinite. God asks for the heart, but the
-        heart is oppressed with uncertainty in its own twilight. God asks for
-        faith, and the heart is not sure of its own faith. It is good that there
-        is a dawn of decision for the sight of the heart; deeds to objectify
-        faith, definite forms to verify belief.
-      </p>
-      <p>
-        The heart is often a lonely voice in the marketplace of living. Man may
-        entertain lofty ideals and behave like the ass that, as the saying goes,
-        “carries gold and eats thistles.” The problem of the soul is how to live
-        nobly in an animal environment; how to persuade and train the tongue and
-        the senses to behave in agreement with the insights of the soul.
-      </p>
-      <p>
-        The integrity of life is not exclusively a thing of the heart; it
-        implies more than consciousness of the moral law. The innermost chamber
-        must be guarded at the uttermost outposts. Religion is not the same as
-        spiritualism; what man does in his concrete, physical existence is
-        directly relevant to the divine. Spirituality is the goal, not the way
-        of man. In this world music is played on physical instruments, and to
-        the Jew the mitsvot are the instruments on which the holy is carried
-        out. If man were only mind, worship in thought would be the form in
-        which to commune with god. But man is body and soul, and his goal is so
-        to live that both “his heart and his flesh should sing to the living
-        God.”
-      </p>
-    </>
-  ),
-  '2020-04-05': (
-    <>
-      <h2>
-        Weekly Meditation{' '}
-        <span className="subtitle">
-          From <i>Spirituality for Ministry</i> by Urban T. Holmes, III
-        </span>
-      </h2>
-
-      <p>
-        Many persons, ordained or not, live in a fairly constant state of noise,
-        with their unresolved past and the uncertain present breaking in on
-        them. They lack a still center and it is only for such a quiet point
-        that we can listen attentively. When I was in my first parish, which was
-        located in the middle of the city, a constant stream of indigents came
-        through. One came into my office and wanted to tell me his story. I sat
-        as if to listen but was deeply troubled inside over some issue now long
-        forgotten. I remember I was fiddling with a pencil. The man stopped his
-        story, looked at me and said, “Young Father, the least you can do is
-        listen.” He was right. There was no still center in me.
-      </p>
-      <p>
-        Thomas Merton (1915 – 1968), the fascinating Cistercian monk whose
-        writings continue to increase in popularity, found the busy life of a
-        Trappist very disconcerting. Despite the fact that speaking is severely
-        curtailed in a Cistercian monastery, he found the place incredibly
-        noisy. For many years he sought permission to live as a hermit on the
-        property of the monastery. He needed the quiet that he might listen. Too
-        frequently we do not understand the hermit’s discipline, a discipline
-        that needs to be ours in spirit, if not in fact.
-      </p>
-    </>
-  )
-};
-
 const noContentMessage = (
   <>
     <h2>Content unavailable</h2>
@@ -190,9 +98,22 @@ const noContentMessage = (
   </>
 );
 
+const getMeditationForDate = date => {
+  const {subTitle, content} = weeklyMeditations[date];
+
+  return (
+    <>
+      <h2>
+        Weekly Meditation <span className="subtitle">{subTitle}</span>
+      </h2>
+      {content}
+    </>
+  );
+};
+
 const allContentData = [
   {
-    getContent: () => meditationContent[currentWeekDates[0].date],
+    getContent: () => getMeditationForDate(currentWeekDates[0].date),
     id: IDS.MEDITATION,
     title: 'Weekly Meditation'
   },
