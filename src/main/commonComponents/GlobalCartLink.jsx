45592478725a74a25c142c5b9993484848019861
EJlearner <rzrshrp@gmail.com>
2020-07-02 20:26:04 -0400

    change styling of cart link

--- src/main/commonComponents/GlobalCartLink.jsx
+++ src/main/commonComponents/GlobalCartLink.jsx
@@ -1,14 +1,24 @@
 import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
 import shoppingUtils from '../../common/components/Shopping/shoppingUtils';
 import {Context} from '../../stores/GlobalStoreWrapper';
+import {LOGICAL_COLORS} from '../../utils/styleVariables';
 
 const StyledWrapperDiv = styled.div`
   position: fixed;
 
+  border: 1px solid black;
+  border-radius: 50%;
+  padding: 5px;
+  background-color: ${LOGICAL_COLORS.CT_ACCENT};
   bottom: 100px;
   right: 80px;
+
+  a {
+    color: black;
+  }
 `;
 
 function GlobalCartLink() {
@@ -19,7 +29,9 @@ function GlobalCartLink() {
   if (cartHasItems) {
     return (
       <StyledWrapperDiv className="shopping-cart-link">
-        Shopping Cart
+        <Link to>
+          <i className="fa fa-shopping-cart fa-2x" title="Shopping Cart" />
+        </Link>
       </StyledWrapperDiv>
     );
   }
