605837845fd17b5d4cec68e65c4f82b3ce5ccd31
EJlearner <rzrshrp@gmail.com>
2020-08-01 23:26:19 -0400

    update video

--- src/stores/messageVideos.js
+++ src/stores/messageVideos.js
@@ -5,6 +5,17 @@ const {PREACHERS} = constants;
 
 const allVideoData = [
   {
+    date: '2020-08-02',
+    description:
+      'Mark 9:2-10  We are living in a time of chaos and confusion. Therefore, it is necessary that we lift ' +
+      'up our heads and open our eyes to experience the glory that God’s and God’s alone. Today’s message ' +
+      'encourages us to do so!',
+    preacher: PREACHERS.G_YEARGIN,
+    scripture: 'Genesis 28:10-17',
+    title: 'A Glimpse of Glory',
+    videoLink: 'https://www.youtube.com/embed/G-IWmeMJf3s'
+  },
+  {
     date: '2020-07-26',
     description:
       'Genesis 28:10-17.  God’s ways are not our ways. We are not always aware that God is with us, however, ' +
