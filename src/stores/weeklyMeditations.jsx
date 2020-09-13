83afb7c0f4b0398d6048ef74bd4208668aa7f538
April <asdayjones@gmail.com>
2020-09-13 11:50:12 -0400

    weekly meditation update

--- src/stores/weeklyMeditations.jsx
+++ src/stores/weeklyMeditations.jsx
@@ -621,5 +621,27 @@ export default {
         </p>
       </>
     )
+  },
+  '2020-09-13': {
+    subTitle: (
+      <>
+        From <i>Thomas Merton: Contemplative Critic</i> by Henri J. M. Nouwen
+      </>
+    ),
+    content: (
+      <p>
+        “Here we have come back to the compassion that must be formed in one’s
+        heart, a compassion that comes out of a deep experience of solidarity,
+        in which one recognizes that the evil, sin and violence which one sees
+        in the world and in the other, are deeply rooted in one’s own heart.
+        Only when you want to confess this and want to rely on the merciful God
+        who can bring good out of evil are you in a position to receive
+        forgiveness and also to give it to other men and women who threaten you
+        with violence. Precisely because Merton had discovered this nonviolent
+        compassion in his solitude could he in a real sense be a monk, that is
+        to say, one who unmasks through his criticism the illusions of a violent
+        society and who wants to change the world in spirit and truth.”
+      </p>
+    )
   }
 };
