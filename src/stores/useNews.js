06a6272143b8947d063031b91924666ddd8b911c
EJlearner <rzrshrp@gmail.com>
2021-06-19 21:44:06 -0400

    add news

--- src/stores/useNews.js
+++ src/stores/useNews.js
@@ -8,6 +8,13 @@ import constants from '../utils/constants';
 function useNews() {
   const allNews = [
     {
+      expireDate: '2021-07-06',
+      id: 'vbs-registration-landing',
+      linkPath: routePaths.CE_VBS_REG_LANDING,
+      postedDate: '2021-06-19',
+      text: 'Vaction Bible School Is Virtual This Year! Click Here To Register'
+    },
+    {
       expireDate: '2021-06-30',
       id: 'cash-app',
       postedDate: '2020-03-03',
@@ -29,42 +36,10 @@ function useNews() {
       text: 'Due to COVID-19, no in-sanctuary service will be held. Watch Sunday’s sermon.'
     },
     {
-      expireDate: '2020-06-01',
-      id: 'coronavirus-info-posted',
-      linkPath: routePaths.MAIN_CORONAVIRUS,
-      postedDate: '2020-04-04',
-      text: 'Coronavirus information has been updated. Visit the Coronavirus Update page for more information.'
-    },
-    {
-      expireDate: '2020-04-04',
-      id: 'anniversary-patrons',
-      postedDate: '2020-03-19',
-      text:
-        'The Ushers and Nurses will be selling patrons for their upcoming anniversary. ' +
-        'The cost of the patrons is $2.00 per name. See any usher or nurse for more information. ' +
-        'Thank you for your continued support.'
-    },
-    {
       expireDate: '2020-05-01',
       id: 'website-redesign',
       postedDate: '2020-03-19',
       text: 'Website redesign is live!'
-    },
-    {
-      expireDate: '2020-06-01',
-      id: 'church-covenant',
-      postedDate: '2020-04-04',
-      text:
-        'Find the Church Covenant and Church Vision on the About Us page for use during ' +
-        'Communion Sunday.'
-    },
-    {
-      expireDate: '2020-06-01',
-      id: 'scriptures-meditation',
-      postedDate: '2020-04-04',
-      text:
-        'Daily scriptures and weekly meditation are posted weekly. Find the link in the ' +
-        'footer menu bar below.'
     }
   ];
 
