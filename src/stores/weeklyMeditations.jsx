36c644e64d6a6a5d229ab5108a72711471be0cdb
EJlearner <rzrshrp@gmail.com>
2022-01-25 18:59:54 -0500

    updated weekly meditation

--- src/stores/weeklyMeditations.jsx
+++ src/stores/weeklyMeditations.jsx
@@ -2379,5 +2379,27 @@ export default {
         a peace that sings and loves and burns and contemplates.”
       </p>
     )
+  },
+  '2022-01-23': {
+    subTitle: (
+      <>
+        From <i>Power Through Constructive Thinking </i> by Emmet Fox
+      </>
+    ),
+    content: (
+      <p>
+        “Sooner or later you will have to put God first in your life, that is to
+        say, your own true spiritual development must become the only thing that
+        really matters. It need not, perhaps had better not, be the only thing
+        in your life, but it must be the first thing. When this happens you will
+        find that you have gotten rid of a great deal of the unnecessary junk
+        that most people carry about; mental junk, of course, although physical
+        junk is very apt to follow upon this. You will find that you will do a
+        great deal less running about after things that do not matter and only
+        waste your time and energy, when once you have put God first. Your life
+        will become simpler and quieter, but in the true sense, richer and
+        infinitely more worth while.”
+      </p>
+    )
   }
 };
