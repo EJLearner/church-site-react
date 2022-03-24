b83857cd44add424b1fc136f81bfeafc366bfc63
EJlearner <rzrshrp@gmail.com>
2022-03-24 19:38:06 -0400

    update firebase to v8

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
-import firebase from 'firebase';
+// This import loads the firebase namespace.
+import firebase from 'firebase/app';
 
-// TODO: Switch to smaller format once I figure out how to do jest mocking this way
-// import firebase from 'firebase/app';
-// import 'firebase/database';
-// import 'firebase/auth';
+// These imports load individual services into the firebase namespace.
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyAzTR9pqe-BTlPl_NrZLN6YqqyNxoLvtos',
