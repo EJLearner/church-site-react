be3289e37a5ffbc9d92aa4f48bc8d5523575cc3c
EJlearner <rzrshrp@gmail.com>
2022-11-21 19:52:57 -0500

    checkpoint

--- src/main/MeditationsPage.jsx
+++ src/main/MeditationsPage.jsx
@@ -1,48 +1,18 @@
 import {format, add} from 'date-fns';
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import {bibleComFormattedVerses} from '../stores/dailyVerses';
 import weeklyMeditations from '../stores/weeklyMeditations';
 import constants from '../utils/constants';
 import {getStartOfWeek} from '../utils/dateTimeUtils';
-import {FONT_FAMILIES} from '../utils/styleVariables';
 
-import BackToTop from './commonComponents/BackToTop';
-import ContentAndSides from './commonComponents/ContentAndSides';
-import ContentAndSubCompassWrapper from './commonComponents/ContentAndSubCompassWrapper';
-import ContentLeftSide from './commonComponents/ContentLeftSide';
-import ContentRightSide from './commonComponents/ContentRightSide';
-import ContentWrapper from './commonComponents/ContentWrapper';
-import SideMenu from './commonComponents/SideMenu';
-import StandardPageWrapper from './commonComponents/StandardPageWrapper';
 import Verse from './commonComponents/Verse';
 
-const StyleWrapper = styled.div`
-  h2 {
-    margin-bottom: 0;
-
-    .subtitle {
-      display: block;
-      font-size: 16px;
-      text-transform: none;
-    }
-  }
-
-  h3 {
-    font-family: ${FONT_FAMILIES.ARIAL};
-    font-weight: bold;
-  }
-
-  h3:first-of-type {
-    margin-top: 0;
-  }
+const StyledMeditationsPage = styled.div`
+  display: flex;
+  justify-content: center;
 `;
-
-const IDS = {
-  MEDITATION: 'meditation',
-  VERSES: 'verses'
-};
 
 const getCurrentWeekDates = () => {
   const jsSundayTime = getStartOfWeek();
@@ -68,7 +38,12 @@ const currentWeekDates = getCurrentWeekDates();
 function getVersesContent() {
   const sundayDate = currentWeekDates[0].date;
   if (!bibleComFormattedVerses[sundayDate]) {
-    return noContentMessage;
+    return (
+      <>
+        <h2>Content unavailable</h2>
+        <p>Sorry, no content is available this week.</p>
+      </>
+    );
   }
 
   const verses = currentWeekDates.map(({date, day}) => {
@@ -87,84 +62,29 @@ function getVersesContent() {
     );
   });
 
-  return (
-    <div>
-      {verses}
-      <BackToTop />
-    </div>
-  );
+  return <div>{verses}</div>;
 }
 
-const noContentMessage = (
-  <>
-    <h2>Content unavailable</h2>
-    <p>Sorry, no content is available this week.</p>
-  </>
-);
-
-const getMeditationForDate = (date) => {
-  const weeklyMeditationInfo = weeklyMeditations[date];
-
-  if (weeklyMeditationInfo) {
-    const {subTitle, content} = weeklyMeditationInfo;
-
-    return (
-      <>
-        <h2>
-          Weekly Meditation <span className="subtitle">{subTitle}</span>
-        </h2>
-        {content}
-      </>
-    );
-  }
-};
-
-const allContentData = [
-  {
-    getContent: () => getMeditationForDate(currentWeekDates[0].date),
-    id: IDS.MEDITATION,
-    title: 'Weekly Meditation'
-  },
-  {
-    getContent: getVersesContent,
-    id: IDS.VERSES,
-    title: 'Daily Scripture Readings',
-    subLinks: currentWeekDates.map(({day}) => ({title: day, elementId: day}))
-  }
-];
-
 export default function MeditationsPage() {
-  const [contentId, setContentId] = useState(IDS.MEDITATION);
-  const {getContent} = allContentData.find(({id}) => id === contentId);
-  const content = getContent() || noContentMessage;
+  const {subTitle: meditationSubtitle, content: meditationContent} =
+    weeklyMeditations[currentWeekDates[0].date] || {};
 
   return (
-    <StyleWrapper>
-      <StandardPageWrapper>
-        <h1>Scriptures &amp; Meditation</h1>
-        <p>
-          As with any relationship, in order to cultivate and nurture a
-          relationship with God, we must spend time. We spend time with God when
-          we read His word and meditate on it while listening for the still,
-          quiet voice. Join us in participating in our daily devotional and
-          weekly meditations as we continue to strengthen our relationship with
-          God through our savior Jesus Christ.
-        </p>
-        <ContentAndSubCompassWrapper>
-          <ContentAndSides>
-            <ContentLeftSide>
-              <SideMenu
-                currentId={contentId}
-                menuData={allContentData}
-                onClick={(id) => setContentId(id)}
-                title="Meditation and Scriptures"
-              />
-            </ContentLeftSide>
-            <ContentWrapper>{content}</ContentWrapper>
-            <ContentRightSide />
-          </ContentAndSides>
-        </ContentAndSubCompassWrapper>
-      </StandardPageWrapper>
-    </StyleWrapper>
+    <StyledMeditationsPage>
+      <div className="daily-scriptures">
+        <h1>Daily Scripture Readings</h1>
+        {getVersesContent()}
+      </div>
+
+      <div className="weekly-meditation">
+        <h1>Weekly Meditation</h1>
+        {meditationContent && (
+          <>
+            <h3>{meditationSubtitle}</h3>
+            {meditationContent}
+          </>
+        )}
+      </div>
+    </StyledMeditationsPage>
   );
 }
