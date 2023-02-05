c71ca7867ff9f95935ec07f45cf0f8e9a22480f4
EJlearner <rzrshrp@gmail.com>
2023-02-05 13:58:20 -0500

    update

--- src/stores/messageVideos.js
+++ src/stores/messageVideos.js
@@ -5,12 +5,20 @@ const {PREACHERS} = constants;
 
 const messageVideos = [
   {
+    date: '2023-02-05T09:00:00',
+    youtubeId: '8jpUXI4sP4E',
+    title: 'Lessons From Our Foreparents:When Our Backs Are Against The Wall!!',
+    preacher: PREACHERS.G_YEARGIN,
+    scripture: 'John 14:1-6 (NLT)'
+  },
+  {
     date: '2023-01-29T09:00:00',
     youtubeId: '-REZwyMx8Uc',
     title: 'Walking On Water..Really!!',
     preacher: PREACHERS.G_YEARGIN,
     scripture: 'Matthew 14:22-32 (NLT)'
-  },  {
+  },
+  {
     date: '2023-01-22T09:00:00',
     youtubeId: 'RanqbjPkdlI',
     title: 'When the Human Condition Meets Unconditional Love',
