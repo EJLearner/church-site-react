7a6a3e6737fb4084221a827195edbd86567b31b2
EJlearner <rzrshrp@gmail.com>
2023-11-11 22:23:38 -0500

    revert homepage to normal

--- src/main/HomePage.jsx
+++ src/main/HomePage.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 import choir from '../assets/images/choir.jpg';
-import yearginFuneralYoutubeImgSrc from '../assets/images/yeargin-funeral-youtube.jpg';
-// import routePaths from '../routePaths';
+import routePaths from '../routePaths';
 
-// import Anchor from './commonComponents/Anchor';
+import Anchor from './commonComponents/Anchor';
 import MainMenubar from './commonComponents/MainMenubar';
 
 const StyledHomePage = styled.div`
   background: var(--black);
-  // TODO: temporary while funeral screen is up - replace afterwards
-  /* background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
-    url(${choir}); */
+  background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
+    url(${choir});
   background-size: cover;
   background-repeat: no-repeat;
   min-height: 100%;
@@ -73,35 +71,11 @@ const StyledHomePage = styled.div`
   }
 `;
 
-const ImageWrapperLink = styled.a`
-  display: block;
-  align-self: center;
-`;
-
-const StyledImage = styled.img`
-  align-self: center;
-  display: block;
-  max-height: calc(90vh - 120px);
-  max-width: 100vw;
-  margin: 0px 0 50px 0;
-`;
-
-const ScreenReaderText = styled.div`
-  position: absolute;
-  left: -10000px;
-  top: auto;
-  width: 1px;
-  height: 1px;
-  overflow: hidden;
-`;
-
-// TODO: temporary while funeral screen is up - replace afterwards
 function HomePage() {
   return (
     <StyledHomePage>
       <MainMenubar />
-
-      {/* <div className="content-wrapper">
+      <div className="content-wrapper">
         <div className="content">
           <h1>
             Welcome to the
@@ -119,42 +93,6 @@ function HomePage() {
             </Anchor>
           </div>
         </div>
-      </div> */}
-
-      <div className="content-wrapper">
-        <ImageWrapperLink
-          href="https://youtube.com/live/kpxlj-G9IJU"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <ScreenReaderText>
-            <p>
-              Family Hour and Musical Portion - Friday, November 10, 2023 5:00 -
-              7:00 PM City Temple of Baltimore Baptist 317 Dolphin Street,
-              Baltimore MD 21217.
-            </p>
-            <p>
-              Friday, November 10th. Live stream of the musical portion of the
-              service will begin at aproximately 6:30 pm.
-            </p>
-            <p>
-              Memorial Service Saturday, November 11, 2023 11:00 AM City Temple
-              of Baltimore Baptist 317 Dolphin Street, Baltimore MD 21217.
-            </p>
-            <p>
-              Saturday, November 11th. Live stream of the memorial service will
-              begin at aproximately 6:30 pm.
-            </p>
-            <p>
-              Service can be accessed via the following youtube link
-              https://www.youtube.com/@thecitytempleofbaltimore3605
-            </p>
-          </ScreenReaderText>
-          <StyledImage
-            alt="Family Hour and Memorial Service picture and information"
-            src={yearginFuneralYoutubeImgSrc}
-          />
-        </ImageWrapperLink>
       </div>
     </StyledHomePage>
   );
