06245a87d67d698b985a703d27330e6394ab7894
EJlearner <rzrshrp@gmail.com>
2024-03-25 21:37:14 -0400

    remove extra files

--- src/main/commonComponents/StandardPageWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-import {FONT_FAMILIES} from '../../utils/styleVariables';
-
-const StandardPageWrapper = styled.div`
-  font-family: ${FONT_FAMILIES.ARIAL};
-`;
-
-export default StandardPageWrapper;
