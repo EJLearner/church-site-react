e7c6b3bdb7d4ca479efb5acfcb3e1d39c82b9ca5
EJlearner <rzrshrp@gmail.com>
2024-06-30 13:22:30 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import react from '@vitejs/plugin-react';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
-import {defineConfig} from 'vite';
+import {defineConfig} from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: browserslistToEsbuild(),
   },
-  server: true,
+  server: {open: true},
   plugins: [react()],
   test: {
-    global: true,
+    globals: true,
     environment: 'jsdom',
     setupFiles: 'setupTest.js',
   },
