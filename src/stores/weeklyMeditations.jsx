2e30b3a7af6c5c2467876557cb9f97ae0e5b222e
April Jones <asdayjones@gmail.com>
2024-07-19 21:57:24 -0400

    updated weekly meditation

--- src/stores/weeklyMeditations.jsx
+++ src/stores/weeklyMeditations.jsx
@@ -1737,4 +1737,24 @@ export default {
       </p>
     ),
   },
+  '2024-07-21': {
+    subTitle: (
+      <>
+        From <i>A Cry for Mercy</i> by Henri J. M. Nouwen
+      </>
+    ),
+    content: (
+      <p>
+        “I call to you, O Lord, from my quiet darkness. Show me your mercy and
+        love. Let me see your face, hear your voice, touch the hem of your
+        cloak. I want to love you, be with you, speak to you and simply stand in
+        your presence. But I cannot make it happen. Pressing my eyes against my
+        hands is not praying, and reading about your presence is not living in
+        it. But there is that moment in which you will come to me, as you did
+        you your fearful disciples, and say, “Do not be afraid; it is I.” Let
+        that moment come soon, O Lord. And if you want to delay it, then make me
+        patient. Amen.”
+      </p>
+    ),
+  },
 };
